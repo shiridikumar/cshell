75e39ed95cf9b7a232025664fcf5210d5f0e3d5d
shiridikumar <p.shiridi@research.iiit.ac.in>
2021-09-13 13:03:03 +0530

    updated system commnd for foreground and background processes

--- syscom.c
+++ syscom.c
@@ -1,15 +1,108 @@
 #include "headers.h"
 
-void syscom(char *c,char **arr, int comm){
+void syscom(char *c, char **arr, int comm)
+{
     char *command;
-    strcpy(command,c);
-
-    int x=fork();
-
-    if(x==0){
-        printf("process %d\n",getpid());
-        execvp(arr[0],NULL);
+    strcpy(command, c);
+    char **args;
+    int x = 111;
+    args = (char **)malloc((comm + 1) * sizeof(char **));
+    int count = 0;
+    for (int j = 0; j < comm; j++)
+    {
+        if (strcmp(arr[j], "&") != 0)
+        {
+            args[count++] = arr[j];
+        }
     }
-    printf("%d",x);
+    args[count] = NULL;
+    if (comm > 3)
+    {
+        printf("Invalid no of arguments\n");
+    }
+    else
+    {
+        if (comm == 3)
+        {
+            if (strcmp(arr[2], "&") == 0)
+            {
+                //background process
+                x = fork();
+                if (x == 0)
+                {
+                    execvp(args[0], args);
+                }
+            }
+            else if (strcmp(arr[1], "&") == 0)
+            {
+                //backgorund process
+                x = fork();
+                if (x == 0)
+                {
+                    execvp(args[0], args);
+                }
+            }
+            else
+            {
+                //fore ground process
+                x = fork();
+                if (x == 0)
+                {
+                    execvp(args[0], args);
+                }
+                else
+                {
+                    wait();
+                }
+            }
+        }
+        else if (comm == 2)
+        {
+            if (strcmp(arr[1], "&") == 0)
+            {
+                //background process
+                x = fork();
+                if (x == 0)
+                {
+                    execvp(args[0], args);
+                    exit(1);
+                }
+                else{
+                    printf("printf %d child id\n",x);
+                }
+            }
+            else
+            {
+                printf("foreground\n");
+                //foreground process
+                x = fork();
+                printf("%d", x);
+                if (x == 0)
+                {
+                    printf("Child process id :%d", getpid());
+                    execvp(args[0], args);
+                }
+                else
+                {
+                    printf("child process id %d",x);
+                    printf("This is parent prcess\n");
+                    wait();
+                    printf("Im not waiting child process\n");
+                }
+            }
+        }
+        else
+        {
+            x = fork();
+            if (x == 0)
+            {
+                execvp(args[0], args);
+            }
+            else
+            {
+                wait();
+            }
+            //foreground  process
+        }
+    }
 }
-
